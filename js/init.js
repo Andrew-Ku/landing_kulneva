5209bba5b7b8c56ae626f9f55d86974dc3301a6f
Andrew-Ku <cool-andrew@yandex.ru>
2017-05-04 22:04:23 +0300

    first commit

---
+++ js/init.js
@@ -0,0 +1,39 @@
+(function ($) {
+    $(function () {
+
+        $('.button-collapse').sideNav();
+        $('.carousel.carousel-slider').carousel({
+            fullWidth: true
+        });
+
+
+        // Клик для просмотра видео из слайдера
+        $('#index-work-sample').find('img').on('click', function (e) {
+
+            $('body').append('<div id="video-sample-modal" class="modal ku-video-modal"></div>');
+
+            $('#video-sample-modal').modal({
+                startingTop: '35%',
+                complete: function () {
+                    $('#video-sample-modal').remove();
+                }
+            });
+            $('#video-sample-modal').modal('open');
+
+            $('#video-sample-modal').html('<div class="video-container"><iframe width="100%" height="100%" src=' + '"' + e.currentTarget.dataset.url + '"' + ' frameborder="0" allowfullscreen></iframe></div>');
+
+        });
+
+        // Скрол фаер
+        var options = [
+            {
+                selector: '.testFire',
+                offset: 200,
+
+                        }
+                ];
+        
+        Materialize.scrollFire(options);
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
